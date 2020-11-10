d2415b4e09ca213533e87612c4c0cfc9738202ad
Nguyen Thanh Luan <50266706+17520722@users.noreply.github.com>
2020-11-10 14:03:13 +0700

    first

---
+++ E-Metro/DangNhap.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace E_Metro
+{
+    /// <summary>
+    /// Interaction logic for DangNhap.xaml
+    /// </summary>
+    public partial class DangNhap : Window
+    {
+        public int re = 0;
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+        }
+
+        public int Login()
+        {
+            if (username_txt.Text.Trim() == "luan" && password_txt.Password.Trim() == "123")
+                return 1;
+            return 0;
+        }
+
+        private void dangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            re = Login();
+            this.Close();
+        }
+        // place holder for taikhoan va matkhau
+
+        private void username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (username_txt.Text == "Tài khoản")
+                username_txt.Text = "";
+        }
+
+        private void username_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(username_txt.Text))
+                username_txt.Text = "Tài khoản";
+        }
+
+        private void passwordv2_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (password_txb.Text == "Mật khẩu")
+                password_txb.Text = "";
+        }
+
+        private void passwordv2_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(password_txt.Password))
+                password_txb.Text = "Mật khẩu";
+        }
+    }
+}
---
+++ E-Metro/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E_Metro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            DangNhap dangnhap = new DangNhap();
+            dangnhap.ShowDialog();
+            if (dangnhap.re == 1)
+            {
+                border_dangNhap_btn.Visibility = Visibility.Hidden;
+                border_dangXuat_btn.Visibility = Visibility.Visible;
+            }            
+            this.Show();
+        }
+
+        private void dangXuat_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
